84022e7e5e81af5dfddbd2687a03beea986ff065
zissysilber <zissysilber@gmail.com>
2023-11-06 22:07:30 -0500

    assign random pictures to buttons

--- PointToPointApp/PointToPointApp/PointToPointApp/PointToPoint.cs
+++ PointToPointApp/PointToPointApp/PointToPointApp/PointToPoint.cs
@@ -17,20 +17,34 @@ namespace PointToPointApp
         List<Image> lstimage;
         bool pointcardflipped = false;
         bool pointpointcardflipped = false;
-        enum ButtonValue {a, b, c, d, e, f, g, h };
+        PictureBox picture1;
+        int imagename = 0;
+
+        enum ButtonValue { a, b, c, d, e, f, g, h };
 
         Random rnd = new();
-        
+
 
         public frmPointToPoint()
         {
             InitializeComponent();
-            btnPoint1.Click += BtnPoint1_Click;
+            btnPoint1.Click += BtnPoint_Click;
+            btnPoint2.Click += BtnPoint_Click;
+            btnPoint3.Click += BtnPoint_Click;
+            btnPoint4.Click += BtnPoint_Click;
+            btnPoint5.Click += BtnPoint_Click;
+            btnPoint6.Click += BtnPoint_Click;
+            btnPoint7.Click += BtnPoint_Click;
+            btnPoint8.Click += BtnPoint_Click;
             btnReset.Click += BtnReset_Click;
+
+
+
 
             lstpointbutton = new() { btnPoint1, btnPoint2, btnPoint3, btnPoint4, btnPoint5, btnPoint6, btnPoint7, btnPoint8 };
             lstimage = new()
             {
+
                 Image.FromFile(path + "Ari Hakadosh.jpg"),
                 Image.FromFile(path + "Churva.jpg"),
                 Image.FromFile(path + "Kever Rochel.jpg"),
@@ -41,74 +55,78 @@ namespace PointToPointApp
                 Image.FromFile(path + "Yam Hamelech.jpg")
             };
 
-        
+        }
+
+        private void BtnPoint2_Click(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        //assign a random image to each button and store the name of the image to be referenced to check if it matches
+        private void AssignValue()
+        {
+            List<Image> usedlist = new();
+            foreach (Button btn in lstpointbutton)
+            {
+                while (btn.BackgroundImage == null)
+                {
+                    Image newimage = lstimage[rnd.Next(0, lstimage.Count())];
+                    newimage.Tag = imagename + 1;
+                    if (usedlist.Where(i => i == newimage).Count() == 0)
+                    {
+                        usedlist.Add(newimage);
+                        btn.BackgroundImage = newimage;
+                        btn.BackgroundImageLayout = ImageLayout.Zoom;
+                        
+                    }
+                }
+
+                
+            }
+
+        }
+
+
+        private void FlipCard()
+        {
+            if (pointcardflipped == true)
+            {
+                AssignValue();
+
+            }
+            pointcardflipped = false;
+        }
+
+
+        private void HidePictures()
+        {
+            DateTime starttime = DateTime.Now;
+            if (pointcardflipped == true)
+                //&& pointpointcardflipped == true)
+
+                while ((DateTime.Now - starttime).TotalSeconds <= 10)
+                {
+                    Application.DoEvents();
+                }
+            btnPoint1.BackgroundImage = null;
         }
 
         private void BtnReset_Click(object? sender, EventArgs e)
         {
-            
-        }
-
-        //assign a random image to each button and store the name of the image to be referenced to check if it matches
-        private void AssignValue(Button btn)
-        {
-            /*
-             * assign random image
-             * assign to button background image
-             * add image to used list
-             * 
-             * choose another image
-             * check if already on used list
-             * if it is, choose another image
-             * if not, assign image to next item in button list
-             * add image to used list
-             * repeat from step 4
-             */
-
-            List<Image> usedlist = new();
-            btn.BackgroundImage = lstimage[rnd.Next(0, lstimage.Count())];
-            usedlist.Add(btn.BackgroundImage);
-
-
-
-                
-               
-
-
-                
-
-        }
-        
-
-        private void FlipCard() { 
-        if(pointcardflipped == true)
+            AssignValue();
+            foreach (Button btn in lstpointbutton)
             {
-                btnPoint1.BackgroundImage = Image.FromFile(path + "Ari Hakadosh.jpg");
-                btnPoint1.BackgroundImageLayout = ImageLayout.Zoom;
+                btn.ForeColor    = Color.LightSkyBlue;
             }
-        else    btnPoint1.BackgroundImage = null;
-            
         }
-        private void BtnPoint1_Click(object? sender, EventArgs e)
+        private void BtnPoint_Click(object? sender, EventArgs e)
         {
-            
-            pointcardflipped = !pointcardflipped;
+            pointcardflipped = true;
             FlipCard();
             HidePictures();
-           
-        }
 
-        private void HidePictures() {
-            DateTime starttime = DateTime.Now;
-            if (pointcardflipped==true) 
-                //&& pointpointcardflipped == true)
-                
-            while ((DateTime.Now - starttime).TotalSeconds <= 10)
-            {
-                Application.DoEvents();
-                }
-            btnPoint1.BackgroundImage = null;
         }
-        
     }
 }
--- PointToPointApp/PointToPointApp/PointToPointApp/Properties/Resources.Designer.cs
+++ PointToPointApp/PointToPointApp/PointToPointApp/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace PointToPointApp.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Ari_Hakadosh {
+            get {
+                object obj = ResourceManager.GetObject("Ari Hakadosh", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
