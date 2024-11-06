7b22a7d211cf896bb3d375cafe9388424f1823a2
zissysilber <zissysilber@gmail.com>
2024-11-06 13:12:14 -0500

    HW 3.11

---
+++ PointToPointApp/PointToPointApp/PointToPointWeb/Pages/Play!jQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PointToPointWeb.Pages
+{
+    public class Play_jQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
